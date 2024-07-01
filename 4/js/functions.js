e7d0d2b7e6b5e834f40ac541bc8859746a8faed9
Keks <keks@htmlacademy.ru>
2024-07-01 18:50:41 +0300

    :heavy_check_mark: Сборка #4

---
+++ 4/js/functions.js
@@ -0,0 +1,46 @@
+// функция проверки длины строки
+function lengthString(string, length) {
+  return string.length <= length;
+}
+
+
+// функция проверки палиндрома
+function isPalindrome(string) {
+  return string.replaceAll(' ', '').toLowerCase() === string.split('').reverse().join('').replaceAll(' ', '').toLowerCase();
+}
+
+
+// функция проверки палиндрома 2
+function isPalindrome2(string) {
+  string = string.replaceAll(' ', '').toLowerCase();
+  for (let i = 0; i < string.length / 2; i++) {
+    if (string[i] !== string[string.length - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+
+// функция фильтрации числа
+function filterNumber(string) {
+  string = String(string).replaceAll(' ', '');
+  let result = '';
+  for (let i = 0; i < string.length; i++) {
+    if (!isNaN(string[i])) {
+      result += string[i];
+    }
+  }
+  return result ? Number(result) : NaN;
+}
+
+
+// функция фильтрации числа 2
+function filterNumber2(string) {
+  return Number(String(string).replaceAll(' ', '').split('').filter((i) => !isNaN(i)).join('')) || NaN;
+}
+
+// функция генерации случайного числа
+const getRandomInt = (min = 0, max = 100) => Math.floor(Math.random() * (Math.floor(max) - Math.ceil(min) + 1) + Math.ceil(min));
+
+export { lengthString, isPalindrome, isPalindrome2, filterNumber, filterNumber2, getRandomInt };
