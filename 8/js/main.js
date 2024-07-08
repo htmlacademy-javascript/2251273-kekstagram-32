64a65d6a8836921b8aa2835b9962262ce4400ece
Keks <keks@htmlacademy.ru>
2024-07-08 18:58:04 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/main.js
@@ -0,0 +1,10 @@
+import { PHOTOS } from './data.js';
+import { drawsThumbnails, drawsThumbnails2 } from './thumbnails.js';
+
+
+// drawsThumbnails(PHOTOS);
+drawsThumbnails2(PHOTOS);
+
+
+export { drawsThumbnails, drawsThumbnails2 };
+
