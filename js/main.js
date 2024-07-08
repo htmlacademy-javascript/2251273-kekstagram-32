5b3dc298b545bec4434341afbd018bc5e669a68e
Echenko <119807032+echenko@users.noreply.github.com>
2024-07-08 20:06:45 +0300

    правки

--- js/main.js
+++ js/main.js
@@ -1,10 +1,7 @@
 import { PHOTOS } from './data.js';
-import { drawsThumbnails, drawsThumbnails2 } from './thumbnails.js';
+import { drawsPhotos } from './photos.js';
 
 
-// drawsThumbnails(PHOTOS);
-drawsThumbnails2(PHOTOS);
+drawsPhotos(PHOTOS);
 
-
-export { drawsThumbnails, drawsThumbnails2 };
 
